7ffb247bb6f34dd76c4abefc9922bf2bf41ba596
dayaa <dayaaravinth@gmaili.com>
2024-02-21 14:08:49 +0530

    driver application

--- app/src/main/java/com/example/driverapplicationtracky/MainActivity.java
+++ app/src/main/java/com/example/driverapplicationtracky/MainActivity.java
@@ -1,28 +1,61 @@
 package com.example.driverapplicationtracky;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextInputLayout email,password;
     Button btn;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        email=findViewById(R.id.getEmail);
+        password=findViewById(R.id.getPassword);
         btn=findViewById(R.id.location);
+
+        firebaseAuth = FirebaseAuth.getInstance();
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, DriverService.class);
-                startActivity(intent);
+                String email1 = email.getEditText().getText().toString().trim();
+                String password1 = password.getEditText().getText().toString().trim();
+
+                loginUser(email1, password1);
             }
         });
+    }
+
+    private void loginUser(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(MainActivity.this, DriverService.class);
+                            startActivity(intent);
+                            Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If login fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
